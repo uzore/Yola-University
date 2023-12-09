07f1750830b3dffc38bccc84aa22b90248c21f5d
uzore <narchjosephs@gmail.com>
2023-12-09 03:20:38 +0100

    Add project files.

---
+++ Yola University/Models/CourseAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Yola_University.Models
+{
+    public class CourseAssignment
+    {
+        [Required] public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Yola University/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yola_University.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
